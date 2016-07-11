f7b1f2a0451ecf791b9b611033042c501a4c5814
kotkoth <darma.kotama@gmail.com>
2016-07-11 14:32:34 +0800

    first excercise

---
+++ first/main.cc
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <cstdlib>
+#include "Sales_item.h"
+
+int main() {
+
+  Sales_item item1, item2;
+
+  std::cin >> item1 >> item2;
+
+  std::cout << item1 + item2 << std::endl;
+  system("pause");
+
+  return 0;
+}
